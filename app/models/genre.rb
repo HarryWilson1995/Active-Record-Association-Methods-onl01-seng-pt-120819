5042b291bfd706a161208487d75a950666c745f4
Harry Wilson <harry.wilson1995a@gmail.com>
2020-03-13 17:30:11 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     arr = []
-    self.artists.all do |artist| 
+    self.artists.map do |artist| 
       arr << artist.name
     end 
     arr 
